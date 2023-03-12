74cee23969505d23fcf017f87efad43a9dde9d67
RoveSuperDev <RoveSuperDev@gmail.com>
2023-03-12 23:54:31 +0800

    xx

--- Pages/01基础语法.xcplaygroundpage/Contents.swift
+++ Pages/01基础语法.xcplaygroundpage/Contents.swift
@@ -2,6 +2,14 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+let number = 10
+
+
+//Tuple 元组
+
+typealias Person = (name: String, age: Int, wallet: Double)
+
+let nick: Person = ("张三",10,2000000000)
+print(nick.0)
 
 //: [Next](@next)
--- Pages/02控制流程.xcplaygroundpage/Contents.swift
+++ Pages/02控制流程.xcplaygroundpage/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+enum HanleType : String {
+    case Cancel = "cancel"
+    case Delete = "delete"
+}
+
+var handleType : HanleType!
+
+handleType = .Delete
+
+
+if(handleType.rawValue.elementsEqual("cancel")){
+    print("处理类型 取消")
+}
